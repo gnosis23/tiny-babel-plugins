ed5fcbc791d0d6fb13e650b84cf36fb06640e274
gnosis23 <bj050323@gmail.com>
2018-09-16 14:00:07 +0800

    babel-template example

---
+++ src/basic/template.js
@@ -0,0 +1,17 @@
+const template = require('babel-template');
+const generate = require('babel-generator').default;
+const t = require('babel-types');
+
+// =============================================================================
+//                                  CASE 1
+// =============================================================================
+const buildRequire = template(`
+  var IMPORT_NAME = require(SOURCE);
+`);
+
+const ast = buildRequire({
+  IMPORT_NAME: t.identifier("MyModule"),
+  SOURCE: t.stringLiteral("my-module")
+});
+
+console.log(generate(ast).code);
