2ca2aedf1b06343a93fd739e8bdb3743ddf7d7aa
gnosis23 <bj050323@gmail.com>
2018-09-24 22:58:23 +0800

    set and map

--- src/basic/set_map.js
+++ src/basic/set_map.js
@@ -1,18 +1,35 @@
+// =============================================================================
+//                                Map & Set
+// =============================================================================
+
+
 // set
+//
+// The Set object lets you store unique values of any type, whether primitive
+// values or object references.
+//
+
+// example 1: key types
 let set = new Set();
 set.add(5);
 set.add("5");
 console.log(set.size); // 2
 
+
+// example 2
 const key1 = {};
 const key2 = {};
 set.add(key1);
 set.add(key2);
 console.log(set.size); // 4
 
+
+// example 3
 const set2 = new Set([1, 2, 3, 4, 5, 5, 5, 5]);
 console.log(set2.size);
 
+
+// example 4: set to array
 function eliminateDuplicates(items) {
   return [...new Set(items)];
 }
@@ -22,7 +39,15 @@ let noDuplicates = eliminateDuplicates(numbers);
 
 console.log(noDuplicates);
 
+
+
 // WeakMap
+//
+// The WeakMap object is a collection of key/value pairs in which the keys are
+// weakly referenced.  The keys must be objects and the values can be arbitrary
+// values.
+//
+//
 let Person = (function() {
 
   let privateData = new WeakMap();
